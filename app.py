f95e92eb13525c3c3735a6bf5c80c0eafc337b2c
razyal8 <razyal8@gmail.com>
2024-02-08 15:40:35 +0200

    get+time

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def hello_world(num):
+    print(num)
     return 'Hello, World!'
+
+@app.route('/get_time')
+def get_time():
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return jsonify({'time': current_time})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
